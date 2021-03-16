fe99e1ec7044f229d87b12b5af405484f3f460e2
Diego <ferreira.dfg@gmail.com>
2021-03-16 20:08:13 -0300

    script

---
+++ 02_Operadores_de_Conjuntos.sql
@@ -0,0 +1,58 @@
+--Exemplo Slide 12
+SELECT employee_id, job_id
+FROM   employees
+UNION
+SELECT employee_id, job_id
+FROM   job_history;
+
+--Exemplo Slide 15
+SELECT employee_id, job_id, department_id
+FROM   employees
+UNION ALL
+SELECT employee_id, job_id, department_id
+FROM   job_history
+ORDER BY  employee_id;
+
+--Exemplo Slide 17
+SELECT employee_id, job_id
+FROM   employees
+INTERSECT
+SELECT employee_id, job_id
+FROM   job_history;
+
+--Exemplo Slide 19
+SELECT employee_id,job_id
+FROM   employees
+MINUS
+SELECT employee_id,job_id
+FROM   job_history;
+
+
+--Exemplo slide 22
+SELECT department_id, TO_NUMBER(null) 
+       location, hire_date
+FROM   employees
+UNION
+SELECT department_id, location_id,  TO_DATE(null)
+FROM   departments;
+
+--Exemplo slide 23
+SELECT employee_id, job_id,salary
+FROM   employees
+UNION
+SELECT employee_id, job_id,0
+FROM   job_history;
+
+--24
+COLUMN a_dummy NOPRINT
+SELECT 'sing' AS "My dream", 3 a_dummy
+FROM dual
+UNION
+SELECT 'I''d like to teach', 1 a_dummy
+FROM dual
+UNION 
+SELECT 'the world to', 2 a_dummy
+FROM dual
+ORDER BY a_dummy;
+
+
