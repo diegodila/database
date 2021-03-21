a42018aa69c5b7d9340989005b46ce5830c78bec
Diego Ferreira Gonçalves <ferreira.dfg@gmail.com>
2021-03-21 15:46:01 -0300

    add new query

--- query.sql
+++ query.sql
@@ -2,6 +2,13 @@ select last_name, department_name
 from employees E join departments D
 on (E.DEPARTMENT_ID=D.DEPARTMENT_ID);
 
+select E.last_name, D.department_name
+from employees E INNER join departments D
+on (E.DEPARTMENT_ID=D.DEPARTMENT_ID);
+
+select E.last_name, D.department_name
+from employees E LEFT OUTER join departments D
+on (E.DEPARTMENT_ID=D.DEPARTMENT_ID);
 
 select E.last_name, D.department_name,
     D.DEPARTMENT_ID PK,
@@ -9,5 +16,50 @@ select E.last_name, D.department_name,
 from employees E INNER join departments D
 on (E.DEPARTMENT_ID=D.DEPARTMENT_ID)
 WHERE DEPARTMENT_NAME = 'Executive'
+or e.last_name = 'Grant'
+or d.department_name = 'NOC';
+
+select E.last_name, D.department_name,
+    D.DEPARTMENT_ID PK,
+    E.DEPARTMENT_ID FK
+from employees E LEFT OUTER join departments D
+on (E.DEPARTMENT_ID=D.DEPARTMENT_ID)
+WHERE DEPARTMENT_NAME = 'Executive'
+or e.last_name = 'Grant'
+or d.department_name = 'NOC';
+
+select E.last_name, D.department_name,
+    D.DEPARTMENT_ID PK,
+    E.DEPARTMENT_ID FK
+from employees E RIGHT join departments D
+on (E.DEPARTMENT_ID=D.DEPARTMENT_ID)
+WHERE DEPARTMENT_NAME = 'Executive'
+or e.last_name = 'Grant'
+or d.department_name = 'NOC';
+
+select E.last_name, D.department_name,
+    D.DEPARTMENT_ID PK,
+    E.DEPARTMENT_ID FK
+from employees E LEFT join departments D
+on (E.DEPARTMENT_ID=D.DEPARTMENT_ID)
+WHERE DEPARTMENT_NAME = null
+or e.last_name = 'Grant'
+or d.department_name = 'NOC';
+
+select E.last_name, D.department_name,
+    D.DEPARTMENT_ID PK,
+    E.DEPARTMENT_ID FK
+from employees E full join departments D
+on (E.DEPARTMENT_ID=D.DEPARTMENT_ID)
+WHERE DEPARTMENT_NAME = "Executive"
+or e.last_name = 'Grant'
+or d.department_name = 'NOC';
+
+select E.last_name, D.department_name,
+    D.DEPARTMENT_ID PK,
+    E.DEPARTMENT_ID FK
+from employees E full join departments D
+on (E.DEPARTMENT_ID=D.DEPARTMENT_ID)
+WHERE DEPARTMENT_NAME = null
 or e.last_name = 'Grant'
 or d.department_name = 'NOC';
