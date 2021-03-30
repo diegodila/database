aa08ab64f4ace495bfd5b530bc51e86656a3447e
Diego Ferreira Gonçalves <ferreira.dfg@gmail.com>
2021-03-30 16:32:41 -0300

    class 03 finish

--- 03/01_Subconsultas.sql
+++ 03/01_Subconsultas.sql
@@ -23,7 +23,7 @@ FROM   employees
 WHERE  salary = 
                 (SELECT MIN(salary)
                  FROM   employees);
-
+-- a função MIN() ou MAX() retorna apenas um resultado, onde o operador = aceita apenas um retorno
 -------------------------------------------------
 SELECT   department_id, MIN(salary)
 FROM     employees
@@ -32,7 +32,15 @@ HAVING   MIN(salary) >
                        (SELECT MIN(salary)
                         FROM   employees
                         WHERE  department_id = 50);
-
+--Where é para coluna, HAVING é para função de grupo
+-------------------------------------------------
+SELECT   department_id, MIN(salary)
+FROM     employees
+GROUP BY department_id
+HAVING   MIN(salary) >
+                       (SELECT MIN(salary)
+                        FROM   employees
+                        WHERE  department_id = 70);
 -------------------------------------------------
 SELECT employee_id, last_name
 FROM   employees
@@ -41,6 +49,16 @@ WHERE  salary =
                  FROM     employees
                  GROUP BY department_id);
 
+--operador usado aceita um numero só. como a subquery retorna uma lista, devemos usar o operador in
+-------------------------------------------------
+SELECT employee_id, last_name
+FROM   employees
+WHERE  salary in
+                (SELECT   MIN(salary)
+                 FROM     employees
+                 GROUP BY department_id);
+
+--operador usado aceita um numero só. como a subquery retorna uma lista, devemos usar o operador in
 -------------------------------------------------
 SELECT last_name, job_id
 FROM   employees
@@ -61,7 +79,16 @@ AND    job_id <> 'IT_PROG';
 -------------------------------------------------
 SELECT employee_id, last_name, job_id, salary
 FROM   employees
-WHERE  salary < ANY
+WHERE  salary  IN
+                    (SELECT salary
+                     FROM   employees
+                     WHERE  job_id = 'IT_PROG')
+AND    job_id <> 'IT_PROG';
+
+-------------------------------------------------
+SELECT employee_id, last_name, job_id, salary
+FROM   employees
+WHERE  salary < all
                     (SELECT salary
                      FROM   employees
                      WHERE  job_id = 'IT_PROG')
@@ -74,3 +101,6 @@ WHERE  emp.employee_id NOT IN
                            (SELECT mgr.manager_id
                             FROM   employees mgr);
 -------------------------------------------------
+select last_name
+from employees
+where department_id=?;
