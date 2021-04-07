b4c670f257b68008dd84c0fe8ab083a90e3f303b
Diego Ferreira Gonçalves <ferreira.dfg@gmail.com>
2021-04-06 21:46:19 -0300

    class 06

---
+++ 06/3.sql
@@ -0,0 +1,52 @@
+--SEQUENCE
+CREATE SEQUENCE dept_deptid_seq;
+
+select pf0645.dept_deptid_seq.nextval
+from dual;
+
+create synonym seq_b for pf0645.dept_deptid_seq;
+
+select seq_b.nextval
+from dual;
+
+select employee_id, last_name
+from employees
+where last_name='Pataballa';
+
+select seq_b.currval
+from dual;
+
+DROP SEQUENCE dept_deptid_seq;;
+
+CREATE SEQUENCE dept_deptid_seq
+                INCREMENT BY 10
+                START WITH 120
+                MAXVALUE 9999
+                NOCACHE
+                NOCYCLE;
+INSERT INTO departments(department_id, department_name, location_id)
+VALUES      (dept_deptid_seq.NEXTVAL, 'Support', 2500);
+
+
+SELECT	dept_deptid_seq.CURRVAL
+FROM	dual;
+
+ALTER SEQUENCE dept_deptid_seq
+               INCREMENT BY 20
+               MAXVALUE 999999
+               NOCACHE
+               NOCYCLE;
+
+DROP SEQUENCE dept_deptid_seq;
+
+--INDEX
+CREATE INDEX 	emp_last_name_idx
+ON 		employees(last_name);
+
+DROP INDEX emp_last_name_idx;
+
+--SYNONYM
+CREATE SYNONYM  d_sum
+FOR  dept_sum_vu;
+
+DROP SYNONYM d_sum;
---
+++ 06/4.sql
@@ -0,0 +1,56 @@
+--VIEWS
+
+CREATE VIEW 	empvu80
+ AS SELECT  employee_id, last_name, salary
+    FROM    employees
+    WHERE   department_id = 80;
+
+select pf0645.dept_deptid_seq.nextval
+from dual;
+
+	
+DESCRIBE empvu80
+
+
+CREATE VIEW 	salvu50
+ AS SELECT  employee_id ID_NUMBER, last_name NAME,
+            salary*12 ANN_SALARY
+    FROM    employees
+    WHERE   department_id = 50;
+
+
+SELECT *
+FROM   salvu50;
+
+
+CREATE OR REPLACE VIEW empvu80
+  (id_number, name, sal, department_id)
+AS SELECT  employee_id, first_name || ' ' 
+           || last_name, salary, department_id
+   FROM    employees
+   WHERE   department_id = 80;
+
+CREATE VIEW	dept_sum_vu
+  (name, minsal, maxsal, avgsal)
+AS SELECT	 d.department_name, MIN(e.salary), 
+             MAX(e.salary),AVG(e.salary)
+   FROM      employees e, departments d
+   WHERE     e.department_id = d.department_id 
+   GROUP BY  d.department_name;
+
+CREATE OR REPLACE VIEW empvu20
+AS SELECT	*
+   FROM     employees
+   WHERE    department_id = 20
+   WITH CHECK OPTION CONSTRAINT empvu20_ck ;
+
+
+CREATE OR REPLACE VIEW empvu10
+    (employee_number, employee_name, job_title)
+AS SELECT	employee_id, last_name, job_id
+   FROM     employees
+   WHERE    department_id = 10
+   WITH READ ONLY ;
+
+
+DROP VIEW empvu80;
